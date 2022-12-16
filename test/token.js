49c08cf603502cae6b042575dbbebcbbbf5a5e3c
m99Tanishq <21bcs164@ietdavv.edu.in>
2022-12-16 07:47:34 +0530

    second test commit

--- test/token.js
+++ test/token.js
@@ -14,4 +14,18 @@ describe("Token contract", function () {
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
         // total balance = 1000
     });
+
+    it("Should transfer tokens between account", async function (){
+        const [owner,addr1,addr2] = await ethers.getSigners();
+
+        const Token = await ethers.getContractFactory("Token"); // instance contract
+
+        const hardhatToken = await Token.deploy(); // deployed
+
+        await hardhatToken.transfer(addr1.address, 10);
+        expect(await hardhatToken.balanceOf(addr1.address)).to.equal(10);
+
+        await hardhatToken.connect(addr1).transfer(addr2.address, 5);
+        expect(await hardhatToken.balanceOf(addr2.address)).to.equal(5);
+    });
 });
